965c97a3bae994dd130ba2a8c790297f3fc13140
Jeremias <Jeremy27xd@gmail.com>
2025-07-17 12:08:58 -0400

    Added an example box that will generate examples when clicked

--- client/src/pages/SentenceStructure.js
+++ client/src/pages/SentenceStructure.js
@@ -2,16 +2,39 @@ import React from 'react';
 import SentenceStructure from '../utils/SentenceChecker/SentenceStructure'
 import SentenceStructurev2 from '../utils/SentenceChecker/SentenceStructurev2'
 import StructureChecker from '../utils/SentenceChecker/StructureChecker'
+import {testCases} from '../utils/SentenceChecker/TestCases'
+
+function SentenceStructures({type = "simple"}) {
+    const filteredExamples = testCases
+    .filter(test => test.structure === type)
+    .map(test => test.sentence); 
+
+  return (
+    <div className="p-6">
+      <h1 style={{ fontSize: "48px" }}>Sentence Structure: {type}</h1>
+      <ExamplesBox examples={filteredExamples} />
+    </div>
+  );
+}
 
 
-function SentenceStructures() {
-    return (
-        <><div>
-            <h1 style={{fontSize: "72px"}}>Sentence Structure Page</h1>
-            <p>Testing Sentence Structure so far, Need to make implentations to webpage.</p>
+function ExamplesBox({examples}) {
+    return(
+        <div style = {{
+            border: "2px solid #ccc",
+            borderRadius: "8px",
+            padding: "16px",
+            backgroundColor: "#f9f9f9",
+            maxWidth: "600px",
+            marginTop: "20px"
+        }}>
+            <h2 style = {{ marginBottom: "10px", color: "#333"}}>Examples:</h2>
+            <ul style={{ paddingLeft: "20px" }}>
+            {examples.map((ex, i) => (
+            <li key={i}>{ex}</li>
+        ))}
+            </ul>
         </div>
-        </>
     )
-};
-
+}
 export default SentenceStructures;
