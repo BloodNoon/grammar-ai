15d5748ad9cb85ba8ac49ac256bd2a161329da12
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-03-26 13:44:15 -0400

    Updated to completed the drop down menus for the subjects

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -8,8 +8,8 @@ const MainNav = () => {
   return (
     <HStack background="rgba(255, 206, 160)" width="100%" spacing={0}>
       <div className="mainNav">
-        
-<Button as={ReactRouterLink} 
+        <Menu>
+        <MenuButton as={ReactRouterLink} 
         to="/sentence-structure" 
         colorScheme='rgb(255, 206, 160)' 
         color="black.500"
@@ -21,9 +21,14 @@ const MainNav = () => {
           color: 'white',
         }}>
           Sentence Structure
-        </Button>
-
-        <Button as={ReactRouterLink} 
+        </MenuButton>
+        <MenuList>
+            <MenuItem as={ReactRouterLink} to="/sentence-structure">Sentence Structure Quiz</MenuItem>
+          </MenuList>
+        </Menu>
+        
+        <Menu>
+        <MenuButton as={ReactRouterLink} 
         to="/adjective-structure" 
         colorScheme='rgb(255, 206, 160)' 
         color="black.500"
@@ -35,9 +40,21 @@ const MainNav = () => {
           color: 'white',
         }}>
           Adjective Lesson
-        </Button>
+            
 
-        <Button as={ReactRouterLink} 
+        </MenuButton>
+        <MenuList>
+            <MenuItem as={ReactRouterLink} to="/adjective-lesson">Introduction to Adjectives</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adjective-royal-order">Adjective Royal Order</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adjective-sentence-structures">Adjective Sentence Structure</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adjective-fill-blanks">Adjective Game</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adjective-quiz">Adjective Quiz</MenuItem>
+          </MenuList>
+          </Menu>
+
+
+        <Menu>
+          <MenuButton as={ReactRouterLink} 
         to="/adverb-structure" 
         colorScheme='rgb(255, 206, 160)' 
         color="black.500"
@@ -49,9 +66,23 @@ const MainNav = () => {
           color: 'white',
         }}>
           Adverb Lesson
-        </Button>
+        </MenuButton>
+         <MenuList>
+            <MenuItem as={ReactRouterLink} to="/adverb-lesson">Introduction to Adverbs</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-types">Adverb Types</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-type-sorting">Adverb Type Sorting</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-forms">Adverb Forms</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-royal-order">Adverb Royal Order</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-sentence-structures">Adverb Sentence Structure</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-sorting-game">Adverb Sorting Game</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-identification-game">Adverb Identification Game</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/adverb-quiz">Adverb Quiz</MenuItem>
+          </MenuList>
+        </Menu>
+        
 
-        <Button as={ReactRouterLink} 
+        <Menu>
+        <MenuButton as={ReactRouterLink} 
         to="/article-structure" 
         colorScheme='rgb(255, 206, 160)' 
         color="black.500" 
@@ -63,9 +94,19 @@ const MainNav = () => {
           color: 'white',
         }}>
           Article Lesson
-        </Button>
-
-        <Button as={ReactRouterLink} 
+        </MenuButton>
+        <MenuList>
+            <MenuItem as={ReactRouterLink} to="/article-lesson">Introduction to Articles</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/article-grammar-legend">Article Grammar</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/article-sentence-builder">Article Sentence Building</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/article-level-selection">Article Level Selector</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/article-wordbank">Article Word Game</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/article-typing-quiz">Article Quiz</MenuItem>
+          </MenuList>
+        </Menu>
+        
+        <Menu>
+        <MenuButton as={ReactRouterLink} 
         to="/verb-tense-structure" 
         colorScheme='rgb(255, 206, 160)' 
         color="black.500"
@@ -78,7 +119,17 @@ const MainNav = () => {
         }}
         >
           Verb Tense Lesson
-        </Button>
+        </MenuButton>
+        <MenuList>
+            <MenuItem as={ReactRouterLink} to="/verb-tense-lesson">Introduction to Verbss</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/verb-grammar-legend">Verb Grammar</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/verb-sentence-builder">Verb Sentence Building</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/verb-level-selection">Verb Level Selector</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/verb-wordbank">Verb Word Game</MenuItem>
+            <MenuItem as={ReactRouterLink} to="/verb-typing-quiz">Verb Quiz</MenuItem>
+          </MenuList>
+        </Menu>
+        
 
         <Menu>
           <MenuButton as={Button}
