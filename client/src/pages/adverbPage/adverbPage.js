b99cbb9bf078bac1773fdad4e4f314a6bd5023a7
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-04-16 13:01:03 -0400

    Update adverbPage.js

--- client/src/pages/adverbPage/adverbPage.js
+++ client/src/pages/adverbPage/adverbPage.js
@@ -134,7 +134,7 @@ const AdverbStructure = () => {
 
           <video controls className="video-player">
             
-              <source src="/Adverbs1.mp4" type="video/mp4"></source>
+              <source src="/adverbs1.mp4" type="video/mp4" />
               Your browser does not support the video tag.
           </video>
         </div>
