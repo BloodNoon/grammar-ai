e59fd4b47ca326e6e826c876ada4f56cbdb2f740
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-04-30 13:15:25 -0400

    Change video height to 480px in prep1Structure.js

--- client/src/pages/Prep1Page/prep1Structure.js
+++ client/src/pages/Prep1Page/prep1Structure.js
@@ -236,7 +236,7 @@ const Prep1Structure = () => {
               </h3>
               
               <div style={{ position: 'relative', width: '100%', backgroundColor: '#000', borderRadius: '8px', overflow: 'hidden' }}>
-                <video controls style={{ width: '100%', height: 'auto', display: 'block' }}>
+                <video controls style={{ width: '100%', height: '480px', display: 'block' }}>
                   <source src="/Lesson6.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
