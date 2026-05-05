5fcec34fb67673da9c586ad52aab3ca7d57c8e85
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-05-05 10:58:23 -0400

    Update ArticleStructure.js

--- client/src/pages/ArticleStructure.js
+++ client/src/pages/ArticleStructure.js
@@ -616,7 +616,7 @@ const ArticleStructure = () => {
                 <video 
                   controls
                   style={{
-                    width: '100%',
+                    width: '100%', 
                     height: '480px',
                     display: 'block'
                   }}
