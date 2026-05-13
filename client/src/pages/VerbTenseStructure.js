61b319ee56b3c72faa5958d95c69f2de8095d0fe
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-05-13 13:50:44 -0400

    Update VerbTenseStructure.js

--- client/src/pages/VerbTenseStructure.js
+++ client/src/pages/VerbTenseStructure.js
@@ -269,7 +269,7 @@ const VerbTenseStructure = () => {
               </Heading>
 
               <Box position="relative" w="100%" bg="black" borderRadius="lg" overflow="hidden" borderWidth="1px" borderColor="gray.300">
-                <video controls style={{ width: '100%', height: 'auto', display: 'block' }}>
+                <video controls style={{ width: '100%', height: '480', display: 'block' }}>
                   <source src="/lesson2.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
