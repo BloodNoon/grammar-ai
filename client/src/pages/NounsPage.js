911214d67d5a0f00a14ed9ebcf022588a56aa6d1
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-05-13 12:49:55 -0400

    Adjust video height in NounsPage component

--- client/src/pages/NounsPage.js
+++ client/src/pages/NounsPage.js
@@ -104,7 +104,7 @@ const NounsPage = () => {
             </Heading>
             
             <Box position="relative" w="100%" bg="black" borderRadius="lg" overflow="hidden" borderWidth="1px" borderColor="gray.300">
-              <video controls style={{ width: '100%', height: 'auto', display: 'block' }}>
+              <video controls style={{ width: '100%', height: '480', display: 'block' }}>
                 <source src="/Lesson1.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
