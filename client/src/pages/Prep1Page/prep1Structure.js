a8e059a16ac148acd8dbe452a6d7f3d8b96995df
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-05-19 13:32:16 -0400

    Update prep1Structure.js

--- client/src/pages/Prep1Page/prep1Structure.js
+++ client/src/pages/Prep1Page/prep1Structure.js
@@ -101,7 +101,7 @@ const Prep1Structure = () => {
             </Heading>
             
             <Box position="relative" w="100%" bg="black" borderRadius="lg" overflow="hidden" borderWidth="1px" borderColor="gray.300">
-              <video controls style={{ width: '100%', height: '580px', display: 'block' }}> 
+              <video controls style={{ width: '100%', height: '480px', display: 'block' }}> 
                 <source src="/Lesson6.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
@@ -133,6 +133,6 @@ const Prep1Structure = () => {
       </Grid>
     </Box>
   );
-};
+
 
 export default Prep1Structure;
