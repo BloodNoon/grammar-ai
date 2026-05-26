3c750231873da6a6c357c8c3d18f958418ecae1f
jaredlansiquot-rgb <jaredlansiquot@gmail.com>
2026-05-26 13:44:06 -0400

    Change video source from Lesson7.mp4 to PrepPhrase.mp4

--- client/src/pages/Prep2Page/prep2Structure.js
+++ client/src/pages/Prep2Page/prep2Structure.js
@@ -172,7 +172,7 @@ const Prep2Structure = () => {
                 controls
                 style={{ width: "100%", height: "580px", display: "block" }}
               >
-                <source src="/Lesson7.mp4" type="video/mp4" />
+                <source src="/PrepPhrase.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </Box>
