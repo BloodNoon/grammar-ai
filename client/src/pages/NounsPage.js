312b74258a782ae70c30f253fae96839003c42df
BNakidia <bnakidia@gmail.com>
2026-05-28 09:56:17 -0400

    Update NounsPage.js
    I added a Learn the Lesson box at the top of the left column on the Introduction to Nouns. The box has a title and directions instructing students to read the boxes, take notes, and watch the video(s). Alongside that, I also added a Practice Makes Perfect box below the video panel and above the practice card on the page. The box basically has a title and directions instructing students to complete each practice in order until all are finished.

--- client/src/pages/NounsPage.js
+++ client/src/pages/NounsPage.js
@@ -130,10 +130,11 @@ const NounsPage = () => {
   return (
     <PageContainer>
       <LessonPageHeader icon="🐸" title="Nouns & Pronouns Builder" />
-      <LessonIntroCard />
 
       <Grid templateColumns={{ base: "1fr", lg: "1fr 1fr" }} gap={8}>
         <VStack spacing={6} align="stretch">
+  
+          {/* Learn the Lesson Box */}
           <GameCard variant="game">
             <Heading size="lg" color="ink.700" mb={2}>
               Learn the Lesson
