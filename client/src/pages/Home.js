f363912a4e7cd46280a9b968071befb242d628b0
BNakidia <bnakidia@gmail.com>
2026-07-16 11:40:47 -0400

    Update Home.js
    I edited the home file in order to include the new "WriteYourSentence" section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import HomeIntro from "../components/HomeIntro";
+import WriteYourSentence from "../components/WriteYourSentence";
 import { VStack, Divider } from "@chakra-ui/react";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <Divider />
         <Hero />
         <HomeIntro />
+        <WriteYourSentence />
       </VStack>
     </Layout>
   );
