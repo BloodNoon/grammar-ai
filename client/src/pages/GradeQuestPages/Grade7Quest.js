b8435458b6778c55054e18ae6e94326f9e35eb0a
BNakidia <bnakidia@gmail.com>
2026-07-31 11:06:26 -0400

    Create Grade7Quest.js
    I just created Great Quest 7.

---
+++ client/src/pages/GradeQuestPages/Grade7Quest.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export default function Grade7Quest() {
+  return (
+    <Layout>
+      <Box p="2rem" textAlign="center">
+        <Heading color="brand.700" mb="1rem">
+          Grade 7 Quest
+        </Heading>
+        <Text color="gray.500">Content coming soon...</Text>
+      </Box>
+    </Layout>
+  );
+}
