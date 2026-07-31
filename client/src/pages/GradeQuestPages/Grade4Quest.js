c22d86f0c3d1a3875a05fbb3d81320fbebb6c2a2
BNakidia <bnakidia@gmail.com>
2026-07-31 11:04:37 -0400

    Create Grade4Quest.js
    I just created Great Quest 4.

---
+++ client/src/pages/GradeQuestPages/Grade4Quest.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export default function Grade4Quest() {
+  return (
+    <Layout>
+      <Box p="2rem" textAlign="center">
+        <Heading color="brand.700" mb="1rem">
+          Grade 4 Quest
+        </Heading>
+        <Text color="gray.500">Content coming soon...</Text>
+      </Box>
+    </Layout>
+  );
+}
